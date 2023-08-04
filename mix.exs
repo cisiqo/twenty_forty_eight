a0d712b13b7d39919104a1f224ef292a2adc7eb0
Jeremy Cowgar <jeremy@cowgar.com>
2023-08-04 19:57:36 -0400

    chore: add test watch command for mix

--- mix.exs
+++ mix.exs
@@ -33,21 +33,22 @@ defmodule TwentyFortyEight.MixProject do
   defp deps do
     [
       {:credo, "~> 1.7"},
-      {:phoenix, "~> 1.7.7"},
-      {:phoenix_html, "~> 3.3"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.19.0"},
-      {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.8.0"},
       {:esbuild, "~> 0.7", runtime: Mix.env() == :dev},
-      {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
-      {:swoosh, "~> 1.3"},
       {:finch, "~> 0.13"},
-      {:telemetry_metrics, "~> 0.6"},
-      {:telemetry_poller, "~> 1.0"},
+      {:floki, ">= 0.30.0", only: :test},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:phoenix, "~> 1.7.7"},
+      {:phoenix_html, "~> 3.3"},
+      {:phoenix_live_dashboard, "~> 0.8.0"},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_live_view, "~> 0.19.0"},
+      {:plug_cowboy, "~> 2.5"},
+      {:swoosh, "~> 1.3"},
+      {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
+      {:telemetry_metrics, "~> 0.6"},
+      {:telemetry_poller, "~> 1.0"}
     ]
   end
 
