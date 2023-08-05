35fa6411bd6ea68ab35d87498f511cca062ad0b1
Jeremy Cowgar <jeremy@cowgar.com>
2023-08-04 20:19:51 -0400

    chore: add documentation to Sequence module

--- lib/twenty_forty_eight/sequence.ex
+++ lib/twenty_forty_eight/sequence.ex
@@ -1,4 +1,16 @@
 defmodule TwentyFortyEight.Sequence do
+  @moduledoc """
+  Apply rules for the 2048 game to a single sequence.
+  The sequence may represent a row or column in the real game.
+  """
+
+  @doc """
+  Combined values to the left. Resulting sequence will be of the same length 
+  as the input sequence but padded with nil values.
+
+  If you need to combined in a different direction, you should prepare the sequence
+  by using Enum.reverse().
+  """
   def combined(sequence) do
     sequence_len = length(sequence)
 
@@ -8,11 +20,14 @@ defmodule TwentyFortyEight.Sequence do
     |> pad_list(sequence_len)
   end
 
-  def combined([], acc), do: acc |> Enum.reverse() |> List.flatten()
-  def combined([h | t], []), do: combined(t, [h])
-  def combined([h | t], [ah | at]) when h == ah, do: combined(t, [[h * 2] | at])
-  def combined([h | t], a), do: combined(t, [h | a])
+  defp combined([], acc), do: acc |> Enum.reverse() |> List.flatten()
+  defp combined([h | t], []), do: combined(t, [h])
+  defp combined([h | t], [ah | at]) when h == ah, do: combined(t, [[h * 2] | at])
+  defp combined([h | t], a), do: combined(t, [h | a])
 
+  @doc """
+  Pad a `list` to a the length of `count` with `value` as the padding character.
+  """
   def pad_list(list, count, value \\ nil) do
     add_count = count - length(list)
     [list | Enum.map(1..add_count//1, fn _ -> value end)] |> List.flatten()
