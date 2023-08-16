4695602425817e1750b5959a5660968904b87be2
Jeremy Cowgar <jeremy@cowgar.com>
2023-08-15 20:23:21 -0400

    feat(web): base UI completed and working!

--- lib/twenty_forty_eight_web/controllers/game.ex
+++ lib/twenty_forty_eight_web/controllers/game.ex
@@ -3,11 +3,29 @@ defmodule TwentyFortyEightWeb.Game do
   Game live view.
   """
 
+  alias TwentyFortyEight.Board
+
   use TwentyFortyEightWeb, :live_view
 
   import TwentyFortyEightWeb.GameComponents
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, page_title: "2048")}
+    board = TwentyFortyEight.Board.new(4, 4)
+
+    {:ok, assign(socket, page_title: "2048", board: board)}
+  end
+
+  def handle_event("move", %{"value" => dir}, socket) do
+    shift_dir =
+      case dir do
+        "left" -> :left
+        "right" -> :right
+        "up" -> :up
+        "down" -> :down
+      end
+
+    board = Board.shift(socket.assigns.board, shift_dir) |> Board.add_random_number()
+
+    {:noreply, assign(socket, board: board)}
   end
 end
--- lib/twenty_forty_eight_web/controllers/game_components.ex
+++ lib/twenty_forty_eight_web/controllers/game_components.ex
@@ -1,11 +1,16 @@
 defmodule TwentyFortyEightWeb.GameComponents do
+  @moduledoc """
+  Supporting components for game play.
+  """
   use Phoenix.Component
 
   attr :value, :integer
 
   def tile(assigns) do
     ~H"""
-    <div class="text-center p-4 text-3xl shadow-lg rounded-lg bg-purple-500"><%= @value %></div>
+    <div class="text-center p-4 text-3xl shadow-lg rounded-lg bg-purple-500 h-16 w-32">
+      <%= @value %>
+    </div>
     """
   end
 end
