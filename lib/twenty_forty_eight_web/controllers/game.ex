8c4705f9e4b0c8e95be1e0a50bb41f05d75caa49
chengshq <csq1124@gmail.com>
2023-11-17 12:05:50 +0800

    feat: add key event

--- lib/twenty_forty_eight_web/controllers/game.ex
+++ lib/twenty_forty_eight_web/controllers/game.ex
@@ -28,4 +28,26 @@ defmodule TwentyFortyEightWeb.Game do
 
     {:noreply, assign(socket, board: board)}
   end
+
+  def handle_event("move", %{"key" => dir}, socket) do
+    shift_dir =
+      case dir do
+        "ArrowUp" -> :up
+        "w" -> :up
+        "ArrowDown" -> :down
+        "s" -> :down
+        "ArrowLeft" -> :left
+        "a" -> :left
+        "ArrowRight" -> :right
+        "d" -> :right
+        _ -> :ok
+      end
+
+    if shift_dir != :ok do
+      board = Board.shift(socket.assigns.board, shift_dir) |> Board.add_random_number()
+      {:noreply, assign(socket, board: board)}
+    else
+      {:noreply, assign(socket, board: socket.assigns.board)}
+    end
+  end
 end
